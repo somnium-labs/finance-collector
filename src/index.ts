c40f2eb84e7cd7ee146a4b5a1f64e8b6e60dfd87
acepm83 <acepm83@limecorp.com>
2020-07-23 21:52:53 +0900

    Added a schedule to run at 16:00 on weekdays

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,25 @@ import * as argparse from 'argparse';
 import KindCollector from '@/provider/kind';
 import NaverFinanceCollector from '@/provider/naver';
 import _ from 'lodash';
+import cron from 'node-cron';
+
+async function update() {
+    const codes = await KindCollector.updateCorpList();
+    await NaverFinanceCollector.updateDailyPrice(_.sortBy(codes, (x) => x, 'asc'));
+}
 
 (async () => {
     try {
         const parser = new argparse.ArgumentParser({
             version: '0.0.1',
             addHelp: true,
-            description: 'example',
+            description: 'Finance Collector',
         });
 
         parser.addArgument(['--init'], {
             required: false,
             nargs: 0,
-            help: 'begin date',
+            help: 'Initialize only the database.',
         });
 
         const args = parser.parseArgs();
@@ -23,8 +29,13 @@ import _ from 'lodash';
         await sequelize.initialize();
 
         if (!args.init) {
-            const codes = await KindCollector.updateCorpList();
-            await NaverFinanceCollector.updateDailyPrice(_.sortBy(codes, (x) => x, 'asc'));
+            // Runs at 4pm on weekdays
+            cron.schedule('0 16 * * 1-5', async () => {
+                await update();
+            });
+
+            // update once
+            await update();
         }
     } catch (e) {
         console.log(e);
